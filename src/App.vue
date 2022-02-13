07fb7d8509b32197b0a90f475ca3a37b987c5ef0
Andrés Corso <andres.corso@gmail.com>
2022-02-12 20:26:35 -0500

    First version

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="info">
+      <span class="data32">Save the date</span>
+      <span class="data20">Mayo 28 2022</span>
+      <span class="data12">Laura y Andrés</span>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'MatriLauraAndres',
+  mounted() {
+    document.title = "Matri Laura y Andrés";
   }
 }
 </script>
@@ -20,7 +23,31 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #722620;
+  color: #F2BC94;
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+}
+.wrapper {
+  height: 100%;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.info {
+  display: flex;
+  flex-direction: column;
+}
+.data32 {
+  font-size: 32px;
+}
+.data20 {
+  font-size: 20px;
+}
+.data12 {
+  font-size: 12px;
 }
 </style>
